c70a62993656e748e471b518bd9ada936bc428b6
Golodhrim <holidayninjastaff@gmail.com>
2023-06-18 02:46:15 -0400

    moved backend from render to railway - I want to see if railway server startups are faster and more consistent

--- constants.ts
+++ constants.ts
@@ -1,8 +1,11 @@
 export const TMDBIMAGE_URL = "https://image.tmdb.org/t/p/original";
 // dev api url
 //export const BINGE_BASE_URL = "http://localhost:5000/api/v1";
-// prod api url
-export const BINGE_BASE_URL = "https://go-bingelist-backend.onrender.com/api/v1";
+// prod api urls
+// Render backend
+//export const BINGE_BASE_URL = "https://go-bingelist-backend.onrender.com/api/v1";
+// Railway backend
+export const BINGE_BASE_URL = "https://go-bingelists-production.up.railway.app/api/v1";
 export const YTEMBED_BASE_URL = "https://youtube.com/embed/";
 
 export const API_HEADER = {
