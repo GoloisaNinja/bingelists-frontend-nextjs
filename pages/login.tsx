2cc53a48a098d792579d7b80574781d5cd04df08
Golodhrim <holidayninjastaff@gmail.com>
2026-09-15 00:52:23 -0400

    various bug fixes and added features for refresh

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,113 +1,134 @@
-import {ChangeEvent, FormEvent, useState, useEffect} from "react";
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 import Head from 'next/head';
-import Link from "next/link";
-import {useRouter} from "next/router";
-import axios, {AxiosResponse} from "axios";
-import {useDispatch, useSelector} from "react-redux";
-import {authenticate, authSelector} from "@/features/auth/authSlice";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import axios, { AxiosResponse } from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { authenticate, authSelector } from '@/features/auth/authSlice';
 import styles from '@/styles/Login.module.scss';
-import {BINGE_BASE_URL} from "@/constants";
-import {useDispatchAlert} from "@/utils/alertFactory";
-import {useMinifiedListLoaders} from "@/utils/initialListLoaders";
-import Spinner from "@/components/spinner";
+import { BINGE_BASE_URL } from '@/constants';
+import { useDispatchAlert } from '@/utils/alertFactory';
+import { useMinifiedListLoaders } from '@/utils/initialListLoaders';
+import Spinner from '@/components/spinner';
 
 export default function Login(): JSX.Element {
-    const dispatch = useDispatch();
-    const {isAuthenticated} = useSelector(authSelector);
-    const {dispatchLoadingMinifiedBingeLists, dispatchLoadingMinifiedFavorites} = useMinifiedListLoaders();
-    const {dispatchAlert} = useDispatchAlert();
-    const router = useRouter();
-    type FormData = {
-        email: string,
-        password: string,
-    }
-    const initialForm: FormData = {
-        email: "",
-        password: "",
-    }
-    const [formData, setFormData] = useState<FormData>(initialForm);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const {email, password} = formData;
+	const dispatch = useDispatch();
+	const { isAuthenticated } = useSelector(authSelector);
+	const {
+		dispatchLoadingMinifiedBingeLists,
+		dispatchLoadingMinifiedFavorites,
+	} = useMinifiedListLoaders();
+	const { dispatchAlert } = useDispatchAlert();
+	const router = useRouter();
+	type FormData = {
+		email: string;
+		password: string;
+	};
+	const initialForm: FormData = {
+		email: '',
+		password: '',
+	};
+	const [formData, setFormData] = useState<FormData>(initialForm);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { email, password } = formData;
 
-    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-        setFormData({...formData, [e.target.name]: e.target.value});
-    }
+	const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setIsLoading(true);
-        const config = {
-            headers: {
-                "Content-type": "application/json",
-            }
-        }
-        const body = JSON.stringify(formData);
-        try {
-            const resp: AxiosResponse = await axios.post(BINGE_BASE_URL + "/user/login", body, config);
-            if (resp.status === 200) {
-                let user = resp.data.data
-                dispatch(authenticate(user));
-                await dispatchLoadingMinifiedBingeLists(user.token.token);
-                await dispatchLoadingMinifiedFavorites(user.token.token);
-                router.push("/trending/landing").then(() => {
-                    dispatchAlert("success", "logged in!");
-                });
-            }
-        } catch (e: any) {
-            setIsLoading(false);
-            let alertMessage = e.message;
-            if (e.hasOwnProperty("response")) {
-                if (e.response.data) {
-                    alertMessage = e.response.data.message;
-                }
-            }
-            dispatchAlert("danger", alertMessage);
-        }
-
-    }
-    useEffect(() => {
-        if (isAuthenticated) {
-            router.push("/trending/landing").then()
-        }
-    },[])
-    return isAuthenticated ? <Spinner /> : (
-        <>
-            <Head>
-                <title>Binge Lists | Login Page</title>
-                <meta name={"description"}
-                      content={"Login to Binge Lists to manage your Movies, Tv, Favourites, and share what to Binge next!"}/>
-            </Head>
-            <div className={styles.login_container}>
-                <div className={styles.bgimg}></div>
-                <div className={styles.form_container}>
-                    <h1><span className={styles.span_blue}>B</span>inge <span
-                        className={styles.span_yellow}>L</span>ogin</h1>
-                    <div className={styles.form_wrapper}>
-                        <form className={styles.login_form} onSubmit={(e) => handleSubmit(e)}>
-                            <label>Email</label>
-                            <input type={"email"}
-                                   name={"email"}
-                                   autoFocus={true}
-                                   autoComplete={"email-address"}
-                                   value={email}
-                                   required={true}
-                                   onChange={(e) => handleInput(e)}
-                            ></input>
-                            <label>Password</label>
-                            <input type={"password"}
-                                   name={"password"}
-                                   autoComplete={"current-password"}
-                                   value={password}
-                                   required={true}
-                                   onChange={(e) => handleInput(e)}
-                            ></input>
-                            <button type={"submit"} className={styles.btn_submit}>login</button>
-                        </form>
-                        <h4>No Account? Register <Link href={"/register"}>here</Link></h4>
-                    </div>
-                </div>
-            </div>
-            {isLoading && (<Spinner/>)}
-        </>
-    );
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setIsLoading(true);
+		const config = {
+			headers: {
+				'Content-type': 'application/json',
+			},
+		};
+		const body = JSON.stringify(formData);
+		try {
+			const resp: AxiosResponse = await axios.post(
+				BINGE_BASE_URL + '/user/login',
+				body,
+				config,
+			);
+			if (resp.status === 200) {
+				let user = resp.data.data;
+				dispatch(authenticate(user));
+				await dispatchLoadingMinifiedBingeLists(user.token.token);
+				await dispatchLoadingMinifiedFavorites(user.token.token);
+				localStorage.setItem('bingeToken', user.token.token);
+				router.push('/trending/landing').then(() => {
+					dispatchAlert('success', 'logged in!');
+				});
+			}
+		} catch (e: any) {
+			setIsLoading(false);
+			let alertMessage = e.message;
+			if (e.hasOwnProperty('response')) {
+				if (e.response.data) {
+					alertMessage = e.response.data.message;
+				}
+			}
+			dispatchAlert('danger', alertMessage);
+		}
+	};
+	useEffect(() => {
+		if (isAuthenticated) {
+			router.push('/trending/landing').then();
+		}
+	}, []);
+	return isAuthenticated ? (
+		<Spinner />
+	) : (
+		<>
+			<Head>
+				<title>Binge Lists | Login Page</title>
+				<meta
+					name={'description'}
+					content={
+						'Login to Binge Lists to manage your Movies, Tv, Favourites, and share what to Binge next!'
+					}
+				/>
+			</Head>
+			<div className={styles.login_container}>
+				<div className={styles.bgimg}></div>
+				<div className={styles.form_container}>
+					<h1>
+						<span className={styles.span_blue}>B</span>inge{' '}
+						<span className={styles.span_yellow}>L</span>ogin
+					</h1>
+					<div className={styles.form_wrapper}>
+						<form
+							className={styles.login_form}
+							onSubmit={(e) => handleSubmit(e)}>
+							<label>Email</label>
+							<input
+								type={'email'}
+								name={'email'}
+								autoFocus={true}
+								autoComplete={'email-address'}
+								value={email}
+								required={true}
+								onChange={(e) => handleInput(e)}></input>
+							<label>Password</label>
+							<input
+								type={'password'}
+								name={'password'}
+								autoComplete={'current-password'}
+								value={password}
+								required={true}
+								onChange={(e) => handleInput(e)}></input>
+							<button type={'submit'} className={styles.btn_submit}>
+								login
+							</button>
+						</form>
+						<h4>
+							No Account? Register <Link href={'/register'}>here</Link>
+						</h4>
+					</div>
+				</div>
+			</div>
+			{isLoading && <Spinner />}
+		</>
+	);
 }
--- utils/useAuthRouteForResponseOrRedirect.ts
+++ utils/useAuthRouteForResponseOrRedirect.ts
@@ -1,49 +1,109 @@
-import React from 'react';
-import {useRouter} from "next/router";
-import {useDispatch, useSelector} from "react-redux";
-import axios from "axios";
-import {authSelector, logout} from "@/features/auth/authSlice";
-import {useDispatchAlert} from "@/utils/alertFactory";
-import useSWR from "swr";
-import {BINGE_BASE_URL} from "@/constants";
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import axios, { AxiosResponse } from 'axios';
+import { authSelector, logout, authenticate } from '@/features/auth/authSlice';
+import { useMinifiedListLoaders } from '@/utils/initialListLoaders';
+import { useDispatchAlert } from '@/utils/alertFactory';
+import useSWR from 'swr';
+import { BINGE_BASE_URL } from '@/constants';
 
 export interface ServerAuthProps {
-    method: string,
-    url: string,
-    body: Object,
+	method: string;
+	url: string;
+	body: Object;
 }
 
-const useAuthRouteForResponseOrRedirect:React.FC<ServerAuthProps> = (props):any => {
-    const router = useRouter();
-    const dispatch = useDispatch();
-    const { dispatchAlert } = useDispatchAlert();
-    const { token, isAuthenticated } = useSelector(authSelector);
-    const fetcher = (url: string) => axios({
-        method: props.method,
-        url,
-        headers: {"Content-type": "application/json", "Authorization": "Bearer " + token.token},
-    }).then((res) => res.data);
-    const { data, error, isLoading, mutate } = useSWR(BINGE_BASE_URL + props.url, fetcher);
-    if (error) {
-        if (error.hasOwnProperty("response")) {
-            let location = window.location;
-            if (error.response.status === 403 && location.href !== 'https://bingelists.app/login') {
-                router.push("/login").then(() => {
-                    if (isAuthenticated) {
-                        dispatch(logout());
-                    }
-                    dispatchAlert("danger", "please authenticate");
-                });
-            }
-        }
-        return {
-            data: null,
-            isLoading: false,
-            mutate,
-        };
-    }
-    return {
-        data, isLoading, mutate
-    };
-}
+const useAuthRouteForResponseOrRedirect: React.FC<ServerAuthProps> = (
+	props,
+): any => {
+	const router = useRouter();
+	const dispatch = useDispatch();
+	const { dispatchAlert } = useDispatchAlert();
+	const { token, isAuthenticated } = useSelector(authSelector);
+	const {
+		dispatchLoadingMinifiedBingeLists,
+		dispatchLoadingMinifiedFavorites,
+	} = useMinifiedListLoaders();
+	let tokenToUse: string = '';
+	let lsToken: any = '';
+	if (typeof window !== 'undefined') {
+		lsToken = localStorage.getItem('bingeToken');
+	}
+	lsToken !== '' ? (tokenToUse = lsToken) : (tokenToUse = token.token);
+	useEffect(() => {
+		const handleUserRefresh = async (t: string) => {
+			if (!t) return;
+
+			const config = {
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: 'Bearer ' + t,
+				},
+			};
+			try {
+				const userResp: AxiosResponse = await axios.get(
+					BINGE_BASE_URL + '/user/refresh',
+					config,
+				);
+				if (userResp.status === 200) {
+					let user = userResp.data.data;
+					dispatch(authenticate(user));
+					dispatchLoadingMinifiedBingeLists(t);
+					dispatchLoadingMinifiedFavorites(t);
+				}
+			} catch (e: any) {
+				let alertMessage = e.message;
+				if (e.hasOwnProperty('response')) {
+					if (e.response.data) {
+						alertMessage = e.response.data.message;
+					}
+				}
+				dispatchAlert('danger', alertMessage);
+			}
+		};
+
+		handleUserRefresh(tokenToUse);
+	}, [tokenToUse]);
+	const fetcher = (url: string) =>
+		axios({
+			method: props.method,
+			url,
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: 'Bearer ' + tokenToUse,
+			},
+		}).then((res) => res.data);
+	const { data, error, isLoading, mutate } = useSWR(
+		BINGE_BASE_URL + props.url,
+		fetcher,
+	);
+	if (error) {
+		if (error.hasOwnProperty('response')) {
+			let location = window.location;
+			if (
+				error.response.status === 403 &&
+				location.href !== 'https://bingelists.app/login'
+			) {
+				router.push('/login').then(() => {
+					if (isAuthenticated) {
+						dispatch(logout());
+					}
+					dispatchAlert('danger', 'please authenticate');
+				});
+			}
+		}
+		return {
+			data: null,
+			isLoading: false,
+			mutate,
+		};
+	}
+
+	return {
+		data,
+		isLoading,
+		mutate,
+	};
+};
 export default useAuthRouteForResponseOrRedirect;
